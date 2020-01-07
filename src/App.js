a80d28bed6f4560ea2629a020ba6fecc49793f90
Janice Huang <janicehuang@gmail.com>
2020-01-07 13:02:55 -0800

    style with font and color gradient to look like Tinder

--- src/App.js
+++ src/App.js
@@ -1,26 +1,32 @@
-import React from 'react';
+import React, { Component } from 'react';
 import logo from './logo.svg';
+import DataViz from './components/DataViz';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  constructor(props) {
+    super();
+    this.state = {
+    }
+  }
+
+  render() {
+
+
+    return (
+      <main className='App'>
+        <header className='App-header'>
+          Match. Select. Taste.
+        </header>
+        <body className='App-body'>
+
+        </body>
+        <footer>
+
+        </footer>
+      </main>
+    );
+  }
 }
 
 export default App;
---
+++ src/components/DataViz.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+import PropTypes from 'prop-types';
+// import axios from 'axios';
+import './DataViz.css'
+class DataViz extends Component {
+  constructor(props) {
+    super();
+
+    this.state = {
+    }
+  }
+}
+export default DataViz;
