94f3bbbe05497ff0df6d11a1196f93e3b525dbd8
Janice Huang <janicehuang@gmail.com>
2020-01-15 14:14:13 -0800

    Updates

---
+++ precache-manifest.08e25692e3e8319a5108225fd9529e81.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2d838ddb3d52a6c942c7deba8eef4764",
+    "url": "/dinder/index.html"
+  },
+  {
+    "revision": "f64b45e4aac0caa0fc55",
+    "url": "/dinder/static/css/main.500fb603.chunk.css"
+  },
+  {
+    "revision": "82cff1b806d2d28c0a6f",
+    "url": "/dinder/static/js/2.23bbc30e.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/dinder/static/js/2.23bbc30e.chunk.js.LICENSE"
+  },
+  {
+    "revision": "f64b45e4aac0caa0fc55",
+    "url": "/dinder/static/js/main.da8c63eb.chunk.js"
+  },
+  {
+    "revision": "7570e23f48fb041d1982",
+    "url": "/dinder/static/js/runtime-main.bf11df02.js"
+  },
+  {
+    "revision": "5d80a88ac83b70c546a9640815a2b82f",
+    "url": "/dinder/static/media/pan-icon-3.5d80a88a.png"
+  }
+]);
