0d890fad96db787a52fc9d1dff4a6c7430f91842
Janice Huang <janicehuang@gmail.com>
2020-01-17 10:59:37 -0800

    turn recipe search results into list of hyperlinks; styling

--- src/components/RecipeList.js
+++ src/components/RecipeList.js
@@ -7,12 +7,16 @@ function RecipeList(props) {
   const recipe_array = list["rec_data"].map(r => r["allData"])
   if (recipe_array !== []) {
     const recipe_list = recipe_array.map((rec) =>
-      <li key={rec["_id"]}>{rec["name"]}</li>);
+      <li key={rec["_id"]}>
+        <a href="{rec['url']}"> {rec["name"]} </a>
+      </li>);
 
   return (
-    <ul className='recipe-list'>{recipe_list}</ul>
-  )
-}
+    <div>
+      <h3 className='recipe-list-header'>RECIPES</h3>
+      <ul className='recipe-list'>{recipe_list}</ul>
+    </div>
+  )}
 }
 
 RecipeList.propTypes = {
